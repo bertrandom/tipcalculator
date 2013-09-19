523db1394fb2cfc5b7d816bce3ac10c0ca680c92
Bertrand Fan <bertrand@fan.net>
2013-09-19 01:58:48 -0700

    Initial import

---
+++ TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Bertrand Fan on 9/19/13.
+//  Copyright (c) 2013 Bertrand Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
